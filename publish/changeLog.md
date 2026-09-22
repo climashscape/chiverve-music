8f574038290c740520290d61300fe29b54ac940c
climashscape <ddx226005215@gmail.com>
2026-09-22 12:49:25 +0800

    chore: 从 lx-music-desktop v2.12.6 整树复制建立基线
    上游基线: lyswhut/lx-music-desktop@ad95d5091c9ed689fa72b5e5c849df65f5a679ce (tag v2.12.6)
    本仓库为独立衍生产品，不再跟随上游 rebase；
    已配置 upstream remote，上游接口修复按需 cherry-pick。

---
+++ publish/changeLog.md
@@ -0,0 +1,15 @@
+目前新项目 Any Listen 的桌面版、Web 版已实现 LX Music 的大部分功能，并额外支持 WebDAV 歌曲播放、WebDAV 数据同步、独立播放列表等功能。
+以后的开发精力将主要集中在新项目上，之前大家在 LX Music 提的功能我们也会考虑在新项目中添加。
+对于日常使用 LX Music 的人可以试试迁移到 Any Listen，若遇到任何问题可以发 issue 反馈。
+Any Listen 的项目地址为 https://github.com/any-listen/any-listen
+
+### 优化
+
+- 优化 tx 推荐歌单列表
+- 优化禁用透明窗口的窗口边框显示效果
+
+### 修复
+
+- 修复打开某些 kg 歌单时歌曲丢失的问题
+- 修复 Windows 7 无法启动的问题
+- 修复禁用透明窗口时按 F11 无法全屏的问题
---
+++ src/main/index.ts
@@ -0,0 +1,37 @@
+import { app } from 'electron'
+import './utils/logInit'
+import '@common/error'
+import {
+  initGlobalData,
+  initSingleInstanceHandle,
+  applyElectronEnvParams,
+  setUserDataPath,
+  registerDeeplink,
+  listenerAppEvent,
+} from './app'
+import { isLinux } from '@common/utils'
+import { initAppSetting } from '@main/app'
+import registerModules from '@main/modules'
+
+// 初始化应用
+const init = () => {
+  console.log('init')
+  if (process.env.BUILD_WIN7 == 'true') import('./utils/winLegacy')
+  void initAppSetting().then(() => {
+    registerModules()
+    global.lx.event_app.app_inited()
+  })
+}
+
+initGlobalData()
+initSingleInstanceHandle()
+applyElectronEnvParams()
+setUserDataPath()
+registerDeeplink(init)
+listenerAppEvent(init)
+
+
+// https://github.com/electron/electron/issues/16809
+void app.whenReady().then(() => {
+  isLinux ? setTimeout(init, 300) : init()
+})
